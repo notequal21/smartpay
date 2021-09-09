1bd640e8c089679130745413395a4bfe2abf05a6
notequal21 <74978665+notequal21@users.noreply.github.com>
2021-09-09 16:04:13 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -59,55 +59,6 @@ if (document.documentElement.clientWidth > 992 && document.querySelector('.wrapp
   });
 
 }
-"use strict"
-
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.querySelector('#form');
-    form.addEventListener('submit', formSend)
-
-    async function formSend(e) {
-        e.preventDefault();
-
-        let error = formValidate(form);
-    }
-
-
-    function formValidate(form) {
-        let error = 0;
-        let formReq = document.querySelectorAll('._req');
-
-        for (let index = 0; index < formReq.length; index++) {
-            const input = formReq[index];
-            formRemoveError(input);
-            
-            if (input.classList.contains('_email')) {
-                if (emailTest(input)) {
-                    formAddError(input);
-                    erorr++;
-                }
-            } else if(input.getAttribute("type" === "checkbox" && input.checked === false)) {
-                formAddError(input);
-                erorr++;
-            } else {
-                if (input.value === '') {
-                    formAddError(input);
-                    erorr++
-                }
-            }
-        }
-    }
-    function formAddError(input) {
-        input.parentElement.classList.add('_error');
-        input.classList.add('_error');
-    }
-    function formRemoveError(input) {
-        input.parentElement.classList.remove('_error');
-        input.classList.remove('_error');
-    }
-    function emailTest(input) {
-        return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
-    }
-}); 
 
 // ---- JQuery
 
